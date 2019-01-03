dbf2a32eef67ac86a7c9d1ccbf50af9fd556efdb
Andreas Pasch <andreas.pasch@sprylab.com>
2019-01-03 16:15:03 +0100

    feature: connect to configured boot node

--- src/blockchain/universal-blockchain.service.ts
+++ src/blockchain/universal-blockchain.service.ts
@@ -1,11 +1,17 @@
-import {Injectable} from "@nestjs/common";
-import {BlockchainService} from "./interfaces/blockchain.interfaces";
-import {Blockchain} from "@kiltprotocol/prototype-sdk";
+import { Blockchain } from "@kiltprotocol/prototype-sdk";
+import { Inject, Injectable } from "@nestjs/common";
+import { ConfigService } from "../config/config.service";
+import { BlockchainService } from "./interfaces/blockchain.interfaces";
 
 @Injectable()
 export class UniversalBlockchainService implements BlockchainService {
 
+  constructor(@Inject('ConfigService') private readonly configService: ConfigService) { }
+
   async connect(): Promise<Blockchain> {
-    return Promise.resolve(Blockchain.build())
+    const bootNodeAddress = this.configService.get('BOOT_NODE_ADDRESS');
+    console.log(`Connecting to  ${bootNodeAddress}`)
+    return Promise.resolve(Blockchain.build(bootNodeAddress))
   }
+
 }
