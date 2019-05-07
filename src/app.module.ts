cc2c619e524b9ee86b88485aba8bc28b88463758
Timo Welde <tjwelde@gmail.com>
2019-05-07 16:19:52 +0200

    feat: add simplest health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { CTypesModule } from './ctypes/ctypes.module'
 import { MessagingModule } from './messaging/messaging.module'
 import { ContactsModule } from './contacts/contacts.module'
 import { BlockchainModule } from './blockchain/blockchain.module'
+import { TerminusModule } from '@nestjs/terminus'
+import { TerminusOptionsService } from './health/terminus-options.service'
 
 @Module({
   imports: [
@@ -12,6 +14,9 @@ import { BlockchainModule } from './blockchain/blockchain.module'
     MessagingModule,
     ContactsModule,
     BlockchainModule,
+    TerminusModule.forRootAsync({
+      useClass: TerminusOptionsService,
+    }),
   ],
 })
 export class AppModule {}
---
+++ src/health/terminus-options.service.ts
@@ -0,0 +1,20 @@
+import {
+  TerminusEndpoint,
+  TerminusOptionsFactory,
+  TerminusModuleOptions,
+} from '@nestjs/terminus'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class TerminusOptionsService implements TerminusOptionsFactory {
+  public createTerminusOptions(): TerminusModuleOptions {
+    const healthEndpoint: TerminusEndpoint = {
+      url: '/health',
+      healthIndicators: [],
+    }
+
+    return {
+      endpoints: [healthEndpoint],
+    }
+  }
+}
