9b4f80fc1117976510a0076954be1aee50a78148
Timo Welde <tjwelde@gmail.com>
2019-07-12 21:52:38 +0200

    fix: linter error

--- src/faucet/exceptions/index.ts
+++ src/faucet/exceptions/index.ts
@@ -2,4 +2,6 @@ export {
   FaucetDropFailedTransferException,
 } from './faucet-drop-failed-transfer'
 export { FaucetDropThrottledException } from './faucet-drop-throttled.exception'
-export { FaucetDropInvalidAddressException } from './faucet-drop-invalid-address.exception'
+export {
+  FaucetDropInvalidAddressException,
+} from './faucet-drop-invalid-address.exception'
