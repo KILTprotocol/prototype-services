fa9afee08025489c2ae601e116782523873cc4a8
Timo Welde <tjwelde@gmail.com>
2020-02-05 11:23:20 +0100

    ci: fail, if tag is found

--- checkForTagOnAws.sh
+++ checkForTagOnAws.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws --region="eu-central-1" ecr list-images --repository-name=$1 | jq '."imageIds"[]["imageTag"]' | grep $2
+aws --region="eu-central-1" ecr list-images --repository-name=$1 | jq '."imageIds"[]["imageTag"]' | grep -q $2 | test $? -eq 1
