9ddb02016b03be6d5e272e8dea7cbef03c71bc2a
Timo Welde <tjwelde@gmail.com>
2020-02-05 15:58:39 +0100

    ci: only execute on releases and cleanup

--- checkForTagOnAws.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-aws --region="eu-central-1" ecr list-images --repository-name=$1 | jq '."imageIds"[]["imageTag"]' | grep -q $2; test $? -eq 1
--- checkForTagOnDocker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -s "https://registry.hub.docker.com/v2/repositories/$1/tags/" | jq '."results"[]["name"]' | grep -q $2; test $? -eq 1
