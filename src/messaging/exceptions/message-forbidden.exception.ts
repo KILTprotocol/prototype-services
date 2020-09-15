22ba830c6bd9e2fc9cf60674e91cc5c6aba21c18
LeonfLK <wenzel@galaniprojects.de>
2020-09-15 14:19:26 +0200

    error message

--- src/messaging/exceptions/message-forbidden.exception.ts
+++ src/messaging/exceptions/message-forbidden.exception.ts
@@ -3,6 +3,9 @@ import { HttpStatus } from '@nestjs/common'
 
 export class ForbiddenMessageAccessException extends HttpException {
   constructor() {
-    super('Message owner signature could not be verified', HttpStatus.FORBIDDEN)
+    super(
+      'Signature could not be verified with the message owner',
+      HttpStatus.FORBIDDEN
+    )
   }
 }
