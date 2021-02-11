9639b4bf493b68598c34132255e5846735cb728d
Timo Welde <tjwelde@gmail.com>
2021-02-11 20:12:25 +0100

    feat: use aws documentdb

--- src/mongoose/mongoose.module.ts
+++ src/mongoose/mongoose.module.ts
@@ -1,6 +1,10 @@
+import fs from 'fs'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '../config/config.module'
 import { ConfigService } from '../config/config.service'
+
+//Specify the Amazon DocumentDB cert
+const ca = [fs.readFileSync(`${__dirname}/rds-combined-ca-bundle.pem`)]
 
 export const MyMongooseModule = MongooseModule.forRootAsync({
   imports: [ConfigModule],
@@ -9,9 +13,12 @@ export const MyMongooseModule = MongooseModule.forRootAsync({
     const user = configService.get('MONGODB_USER')
     const pass = configService.get('MONGODB_PASS')
     return {
-      uri: `mongodb://${host}/registry?authSource=admin`,
+      uri: `mongodb://${host}/devnet?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred`,
       user,
       pass,
+      sslValidate: true,
+      sslCA: ca,
+      useNewUrlParser: true,
     }
   },
   inject: [ConfigService],
